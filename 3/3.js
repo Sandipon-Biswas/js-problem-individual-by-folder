33a805316eb6edcf3d3acca5e4dac62ae48f0224
Sandipon-Biswas <98538000+Sandipon-Biswas@users.noreply.github.com>
2023-03-26 21:03:36 +0600

    3

---
+++ 3/3.js
@@ -0,0 +1,18 @@
+// Write a JS function AreaOfTringle that takes the base and height of a triangle and returns its area.
+// Example 1:
+// Input: 7, 8
+// Output: 14
+// Explanation: Base 7 and height 8. After calculation,its area is 14.
+// Example 2:
+// Input: 10, 10
+// Output: 50  
+
+// Constraints:
+// Base cannot be negative 
+// Height cannot be negative
+
+
+function   AreaOfTringle (B,H){
+{B>0 && H>0 && console.log(R=(B*H)/2);  }   
+}
+AreaOfTringle(60,60)
