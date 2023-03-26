448d5a1d32588f20c1cc1180dfa2ed1decefc682
Sandipon-Biswas <98538000+Sandipon-Biswas@users.noreply.github.com>
2023-03-26 20:48:50 +0600

    first commit

---
+++ 1/1.js
@@ -0,0 +1,12 @@
+// Write a JavaScript Function   printNumber that takes an integer N and prints all the numbers between 0 to N.
+// Example:
+// Input: 5
+// Output: 0 1 2 3 4 5
+// Constraints:
+// 0 < N 
+function  printNumber(N){
+    for(i=0;i<N+1;i++){
+        console.log(i)
+    }
+}
+printNumber(5)
